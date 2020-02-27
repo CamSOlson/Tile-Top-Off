dc23b3d18e6b1ffe2ced6532486847f50a629fb2
Cam Olson <cso7654@ad.rit.edu>
2020-02-27 09:09:52 -0500

    Fixed path complexity output to 2 decimals

--- scripts/menus/start.js
+++ scripts/menus/start.js
@@ -5,20 +5,20 @@ window.addEventListener("load", function() {
 	tempBoardSize = customBoardSize;
 	tempObstacleAmount = customPathComplexity;
 
-	document.querySelector("input#board-size-slider").value = tempBoardSize;
-	document.querySelector("output[for=board-size-slider]").innerHTML = tempBoardSize;
-	document.querySelector("input#path-complexity-slider").value = parent.tempObstacleAmount;
-	document.querySelector("output[for=path-complexity-slider]").innerHTML = tempObstacleAmount;
+	document.querySelector("input#board-size-slider").value = Number(tempBoardSize);
+	document.querySelector("output[for=board-size-slider]").innerHTML = Number(tempBoardSize);
+	document.querySelector("input#path-complexity-slider").value = Number(tempObstacleAmount).toFixed(2);
+	document.querySelector("output[for=path-complexity-slider]").innerHTML = Number(tempObstacleAmount).toFixed(2);
 });
 
 function onBoardSizeSliderInput(){
 	tempBoardSize = document.querySelector("input#board-size-slider").value;
-	document.querySelector("output[for=board-size-slider]").innerHTML = tempBoardSize;
+	document.querySelector("output[for=board-size-slider]").innerHTML = Number(tempBoardSize);
 }
 
 function onObstacleSliderInput(){
 	tempObstacleAmount = document.querySelector("input#path-complexity-slider").value;
-	document.querySelector("output[for=path-complexity-slider]").innerHTML = tempObstacleAmount;
+	document.querySelector("output[for=path-complexity-slider]").innerHTML = Number(tempObstacleAmount).toFixed(2);
 }
 
 function applyCustomOptions(){
